b8a88ff170af94310f5c1f336d04b8fcc541d3c4
armetcal <armetcal@edu.uwaterloo.ca>
2021-11-25 23:50:21 -0800

    Sort codon table

--- BIOL 112 Transcription Translation App/TnT_script.js
+++ BIOL 112 Transcription Translation App/TnT_script.js
@@ -10,22 +10,18 @@ function do_onclick() {
     const total_length = 35;
 
     // Codon table:
-    var Codon = ['AAA', 'AAC', 'AAG', 'AAT', 'ACA', 'ACC', 'ACG',
-        'ACT', 'AGA', 'AGC', 'AGG', 'AGT', 'ATA', 'ATC',
-        'ATG', 'ATT', 'CAA', 'CAC', 'CAG', 'CAT', 'CCA',
-        'CCC', 'CCG', 'CCT', 'CGA', 'CGC', 'CGG', 'CGT',
-        'CTA', 'CTC', 'CTG', 'CTT', 'GAA', 'GAC', 'GAG',
-        'GAT', 'GCA', 'GCC', 'GCG', 'GCT', 'GGA', 'GGC',
-        'GGG', 'GGT', 'GTA', 'GTC', 'GTG', 'GTT', 'TAA',
-        'TAC', 'TAG', 'TAT', 'TCA', 'TCC', 'TCG', 'TCT',
-        'TGA', 'TGC', 'TGG', 'TGT', 'TTA', 'TTC', 'TTG', 'TTT']
-    var AA = ['Lys', 'Asn', 'Lys', 'Asn', 'Thr', 'Thr', 'Thr', 'Thr', 'Arg',
-        'Ser', 'Arg', 'Ser', 'Ile', 'Ile', 'Met', 'Ile', 'Gln', 'His', 'Gln',
-        'His', 'Pro', 'Pro', 'Pro', 'Pro', 'Arg', 'Arg', 'Arg', 'Arg', 'Leu',
-        'Leu', 'Leu', 'Leu', 'Glu', 'Asp', 'Glu', 'Asp', 'Ala', 'Ala', 'Ala',
-        'Ala', 'Gly', 'Gly', 'Gly', 'Gly', 'Val', 'Val', 'Val', 'Val', 'Stp',
-        'Tyr', 'Stp', 'Tyr', 'Ser', 'Ser', 'Ser', 'Ser', 'Stp', 'Cys', 'Trp',
-        'Cys', 'Leu', 'Phe', 'Leu', 'Phe']
+    var Codon = ['GCA','GCC','GCG','GCT','AGA','AGG','CGA','CGC','CGG','CGT',
+    'AAC','AAT','GAC','GAT','TGC','TGT','CAA','CAG','GAA','GAG','GGA','GGC',
+    'GGG','GGT','CAC','CAT','ATA','ATC','ATT','CTA','CTC','CTG','CTT','TTA',
+    'TTG','AAA','AAG','ATG','TTC','TTT','CCA','CCC','CCG','CCT','AGC','AGT',
+    'TCA','TCC','TCG','TCT','TAA','TAG','TGA','ACA','ACC','ACG','ACT','TGG',
+    'TAC','TAT','GTA','GTC','GTG','GTT']
+    var AA = ['Ala','Ala','Ala','Ala','Arg','Arg','Arg','Arg','Arg','Arg','Asn',
+    'Asn','Asp','Asp','Cys','Cys','Gln','Gln','Glu','Glu','Gly','Gly','Gly',
+    'Gly','His','His','Ile','Ile','Ile','Leu','Leu','Leu','Leu','Leu','Leu',
+    'Lys','Lys','Met','Phe','Phe','Pro','Pro','Pro','Pro','Ser','Ser','Ser',
+    'Ser','Ser','Ser','Stp','Stp','Stp','Thr','Thr','Thr','Thr','Trp','Tyr',
+    'Tyr','Val','Val','Val','Val']
     var array2D = new Array(2);
     array2D[0] = Codon;
     array2D[1] = AA;
